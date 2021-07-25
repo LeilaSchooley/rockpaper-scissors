15f7bf50c2776141aff5e64f91062e13e3f536e2
LeilaSchooley <vortex962@zoho.com>
2021-07-25 13:36:02 +0100

    Shortened code added buttons for rock paper scissors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let options = ['Rock','Paper','Scissors']
 
 let playerCounter =1;
-let computerCounter =1;
+let computerCounter = 1;
 
-//console.log(random, options[random]);
+
 function computerPlay(){
     let random = Math.floor(Math.random() * options.length);
 
@@ -11,100 +11,83 @@ function computerPlay(){
 
 }
 
-//console.log(computerPlay())
-
 
 function playRound(playerSelection, computerSelection){
     
     playerSelection = playerSelection.toLowerCase()
 
-    console.log(playerSelection, computerSelection)
-    switch(playerSelection, computerSelection){
-
-        
-        case playerSelection == "rock"  && computerSelection == "Scissors":
-            playerCounter++
-            return("You Win! Rock Beats Scissors!")
-        case computerSelection == "Rock" && playerSelection == "scissors":
-            computerCounter++
-            return("You Lose! Rock Beats Scissors!")
-
-        case(playerSelection =="scissors" && computerSelection == "Paper"):
-            playerCounter++
-
-            return("You Win! Scissors Beats Paper!")
-        case computerSelection == "Scissors" && playerSelection == "paper":
-            computerCounter++
-            return("You Lose! Scissors Beats Paper!")
-
-        case(playerSelection =="paper" && computerSelection == "Rock"):
-            playerCounter++
-
-            return("You Win! Paper Beats Rock!")
-        case computerSelection == "Scissors" && playerSelection == "paper":
-            computerCounter++
-            return("You Lose! Paper Beats Rock!")
-
-
-    }
-
-
-}
-
-function game(playerSelection, computerSelection){
-
-    playerSelection = playerSelection.toLowerCase()
-
-    console.log(playerSelection, computerSelection)
-    
     if (playerSelection == "rock"  && computerSelection == "Scissors"){
-        playerCounter++
+        playerCounter += 1
         return("You Win! Rock Beats Scissors!")
 
     }
 
     else if (computerSelection == "Rock" && playerSelection == "scissors"){
-        computerCounter++
+        computerCounter += 1
         return("You Lose! Rock Beats Scissors!")
     }
 
 
     else if(playerSelection =="scissors" && computerSelection == "Paper"){
-        playerCounter++
+        playerCounter += 1
 
         return("You Win! Scissors Beats Paper!")
 
     }
 
     else if (computerSelection == "Scissors" && playerSelection == "paper"){
-        computerCounter++
+        computerCounter += 1
         return("You Lose! Scissors Beats Paper!")
 
     }
 
 
     else if(playerSelection =="paper" && computerSelection == "Rock"){
-        playerCounter++
+        playerCounter += 1
 
         return("You Win! Paper Beats Rock!")
 
     }
         
     else if (computerSelection == "Paper" && playerSelection == "rock"){
-        computerCounter++
+        computerCounter += 1
         return("You Lose! Paper Beats Rock!")
 
+
+}}
+
+function game(){
+    
+    for (let i = 0; i < 5; i++) {
+        let playerSelection= prompt("Choose Rock, Paper, or Scissors:")
+        console.log(playRound(playerSelection, computerPlay()))
+    
+    }
+
+    console.log(`Computer has ${computerCounter} points!
+You have ${playerCounter} points!`)
+        if (computerCounter > playerCounter){
+            console.log("You Lost!")
+
+
+        }
+    
+        else if (playerCounter > computerCounter){
+            console.log("You Won!")
+
+
+        }
+        else {
+
+            console.log("It's a Tie!");
+        }
+        
+        
     }
         
 
-    }
-
-
-
-
-for (let i = 0; i < 5; i++) {
-    let playerSelection= prompt("Choose Rock, Paper, or Scissors:")
-    
-    console.log(i)
-    console.log(game(playerSelection, computerPlay()))
-}
+let choice = document.querySelectorAll("button")
+console.log(choice)
+choice.forEach(button => button.addEventListener("click", function(){
+    console.log(button)
+}))
